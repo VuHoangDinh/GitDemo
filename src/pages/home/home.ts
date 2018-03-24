4d2d5ed899980a096d22c25ce7e8a40902cd8f11
VuHoangDinh <hdnv91@gmail.com>
2018-03-24 16:36:07 +0200

    s
    a

---
+++ src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AboutPage} from '../about/about';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  navigateToAbout(){
+    this.navCtrl.push(AboutPage);
+  }
+
+  navigateToHome(){
+    this.navCtrl.push(HomePage);
+  }
+
+}
